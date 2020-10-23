aa9f6291846825838ebf9008a4384522e72ac8de
oneheadking <50674351+oneheadking@users.noreply.github.com>
2020-10-23 11:27:45 +0800

    Add diet post route

--- app.js
+++ app.js
@@ -10,8 +10,12 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+app.get("/cuisine", (req, res) => {
   res.render("selectCuisine");
+});
+
+app.get("/diet", (req, res) => {
+  res.render("selectDiet");
 });
 
 //get api: https://api.spoonacular.com/recipes/complexSearch?cuisine=italian&apiKey=4d571645da1d408a9d5b832c5bec6874&diet=vegetarian
@@ -29,18 +33,29 @@ app.post("/cuisine", async (req, res) => {
   //It should be render all information in grid
   res.render("display", {
     recipes: recipes,
-    cuisine: country,
+    broadType: "Cuisine",
+    specificType: country,
     numberOfRecipes: numOfRecipes,
   });
 });
 
-app.get("/cuisine", async (req, res) => {
-  const url = `https://api.spoonacular.com/recipes/${716426}/information?apiKey=4d571645da1d408a9d5b832c5bec6874`;
+app.post("/diet", async (req, res) => {
+  const diet = req.body.dietName;
+  console.log(diet);
+  const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.API_KEY}&diet=${diet}&number=50`;
   const response = await fetch(url);
   const result = await response.json();
   console.log(result);
+  const recipes = result.results;
+  const numOfRecipes = result.number;
+
   //It should be render all information in grid
-  res.send(result);
+  res.render("display", {
+    recipes: recipes,
+    broadType: "Diet",
+    specificType: diet,
+    numberOfRecipes: numOfRecipes,
+  });
 });
 
 app.listen(4000, () => {

e9997767fcbb7be7bb2bd8db144c2dbc7734f40e
oneheadking <50674351+oneheadking@users.noreply.github.com>
2020-10-29 06:24:49 +0800

    Finish broad query arrow toggle element

--- public/js/display.js
+++ public/js/display.js
@@ -6,15 +6,34 @@ const getRecipesHeight = () => {
   if (addedHeight < 1500) {
     addedHeight = 1500; //should be minimum height for footer to stay at bottom
   } else {
-    addedHeight = recipesDOM.offsetHeight + 200;
+    addedHeight = recipesDOM.offsetHeight + 400;
   }
-  alert(recipesDOM.offsetHeight);
   footerDOM.style.position = "absolute";
   footerDOM.style.top = addedHeight + "px";
 };
 
-
 //For zooming to 25%, the footer should be at least top = 3500px;
-
-
 getRecipesHeight();
+
+const toggleBroadQuery = (e) => {
+  const queryDOM = document.querySelectorAll(".queryTag");
+  const spanID = e.id; //get the id of clicked span
+  const arrowDOM = document.querySelector(`#${spanID} i`); //get the arrow DOM under the span
+  // console.log(arrowDOM);
+  // console.log(queryDOM);
+  if (queryDOM.length > 10) {
+    if (arrowDOM.className === "fas fa-angle-up") {
+      // console.log(arrowDOM.className);
+      for (let i = 10; i < queryDOM.length; i++) {
+        queryDOM[i].style.display = "none";
+        arrowDOM.className = "fas fa-angle-down"; //change the up arraow to down arrow
+      }
+    } else {
+      for (let i = 0; i < queryDOM.length; i++) {
+        queryDOM[i].style.display = "block";
+        arrowDOM.className = "fas fa-angle-up";
+      }
+    }
+  }
+  //need toggle the arrow button
+};

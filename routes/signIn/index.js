ab0c9f234bc721012164f962b220ac3b6c1ef16e
Mika Fung <fungcheukman@hotmail.com>
2020-11-06 14:52:10 +0800

    passport done

--- routes/signIn/index.js
+++ routes/signIn/index.js
@@ -8,7 +8,7 @@ router.get('/', (req, res) => {
 
 router.post('/', passport.authenticate('local-login', {
     successRedirect: '/',
-    failureRedirect: 'error'
+    failureRedirect: '/error'
 }));
 
 module.exports = router;
